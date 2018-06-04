50d12147deaedb1e1a0ddeeb19a2de5702c96f88
guotingjie@126.com <guotingjie@126.com>
2018-06-04 17:46:29 +0800

    remove logs

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -55,7 +55,6 @@ export function updateFundPassword(funds_password_status){
 }
 
 export function updatePayStatus(pay_status){
-	console.log(pay_status)
 	return {
 		type: ActionType.UPDATE_PAY_STATSUS,
 		pay_status
